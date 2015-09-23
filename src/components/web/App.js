3f80d2a4e3717c3e4ffd67be4bcde827a1252310
Zach Perrault <zach.perrault@gmail.com>
2015-09-23 11:48:49 -0400

    Let's not go to far with stateless components just yet

--- src/components/web/App.js
+++ src/components/web/App.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => (
-  <div>
-    <p>
-      Welcome to React!
-    </p>
-    <p>
-      To get started, edit src/main.web.js
-    </p>
-  </div>
-)
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Welcome to React!
+        </p>
+        <p>
+          To get started, edit src/main.web.js
+        </p>
+      </div>
+    )
+  }
+}
