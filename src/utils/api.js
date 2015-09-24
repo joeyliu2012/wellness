6c98e29d7b9f9984c6ab6e96f3f3820711c03a5a
Zach Perrault <zach.perrault@gmail.com>
2015-09-24 15:57:42 -0400

    A small start to the client side api module A wrapper around `fetch`

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,34 +1,61 @@
 export default class API {
-  constructor() {
+  constructor(headers = {}) {
+    this.headers = headers
+  }
 
+  delete(uri, data) {
+    return this.request('delete', uri, data, null, this.headers)
+  }
+
+  get(uri, data) {
+    return this.request('get', uri, null, data, this.headers)
+  }
+
+  patch(uri, data) {
+    return this.request('patch', uri, data, null, this.headers)
+  }
+
+  post(uri, data) {
+    return this.request('post', uri, data, null, this.headers)
+  }
+
+  put(uri, data) {
+    return this.request('put', uri, data, null, this.headers)
   }
 
   request(method, uri, body, params, headers) {
-    return fetch(this.addQueryParams(uri, params), {
+    return fetch(API.addQueryParams(uri, params), {
       method,
       headers: {
         'Content-Type': 'application/json',
         ...headers,
       },
-      body: JSON.stringify(body),
+      body: body ? JSON.stringify(body) : null,
     })
       .then((resp) => {
         if (resp.status >= 200 && resp.status < 300) {
           return Promise.resolve(resp)
-        } else {
-          return Promise.reject(resp)
         }
+        return Promise.reject(resp)
       })
       .then((resp) => resp.json())
   }
 
+  addHeaders(newHeaders) {
+    this.headers = {
+      ...this.headers,
+      ...newHeaders,
+    }
+  }
+
   static addQueryParams(uri, params) {
-    const paramString = Object.keys(params)
-                               .map((key) => [key, params[key]])
-                               .map((pair) => pair.join('='))
-                               .join('&')
-    console.log({paramString})
-    return [uri, paramString].join('?')
+    if (!params) return uri
+    return [
+      uri,
+      Object.keys(params)
+            .map((key) => [key, params[key]].join('='))
+            .join('&'),
+    ].join('?')
   }
 }
 
