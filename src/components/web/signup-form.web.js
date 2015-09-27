4b1127138c9f57350ed0e5f9757cace028ed0fcb
Zach Perrault <zach.perrault@gmail.com>
2015-09-27 18:45:22 -0400

    FIxes an error in the users controller where errors would cause the resp to hang.
    Also beefs up the signup form to demo the api module

--- src/components/web/signup-form.web.js
+++ src/components/web/signup-form.web.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 
+import api from 'utils/api'
+
+const INITIAL_STATE = {
+  fullName: null,
+  email: null,
+  password: null,
+}
+
+
 export default class SignupForm extends Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = INITIAL_STATE
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
 
   handleSubmit(evt) {
     evt.preventDefault()
-    console.log('Submit!')
+    api.post('/users', this.state)
+       .then((resp) => this.setState(INITIAL_STATE))
+       .catch((err) => console.error(err))
+  }
+
+  handleInputChange(input) {
+    return ({target: {value}}) => {
+      this.setState({[input]: value})
+    }
   }
 
   render() {
@@ -12,13 +35,22 @@ export default class SignupForm extends Component {
       <form onSubmit={this.handleSubmit}>
         <h4>Create an account</h4>
         <div>
-          <input ref="fullName" type="text" placeholder="Full Name"/>
+          <input onChange={this.handleInputChange('fullName')}
+                 value={this.state.fullName}
+                 type="text"
+                 placeholder="Full Name"/>
         </div>
         <div>
-          <input ref="email" type="email" placeholder="Email"/>
+          <input onChange={this.handleInputChange('email')}
+                 value={this.state.email}
+                 type="email"
+                 placeholder="Email"/>
         </div>
         <div>
-          <input ref="password" type="password" placeholder="Password"/>
+          <input onChange={this.handleInputChange('password')}
+                 value={this.state.password}
+                 type="password"
+                 placeholder="Password"/>
         </div>
         <button type="submit">Submit</button>
       </form>
