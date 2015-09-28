3f478ece0a519aea817ec6dfce7376fb74fb98fb
Zach Perrault <zach.perrault@gmail.com>
2015-09-28 14:34:29 -0400

    Fix naming errors

--- src/reducers/token.js
+++ src/reducers/token.js
@@ -1,12 +1,12 @@
-import { REQUEST_LOGIN, RECEIVE_TOKEN } from 'constants/action-types'
+import { REQUEST_TOKEN, RECEIVE_TOKEN } from 'constants/action-types'
 import { AUTH_HEADER } from 'constants/headers'
 
 
-export default function login(state = {
+export default function token(state = {
   token: localStorage.getItem(AUTH_HEADER),
 }, action) {
   switch (action.type) {
-  case REQUEST_LOGIN:
+  case REQUEST_TOKEN:
     return state
   case RECEIVE_TOKEN:
     localStorage.setItem(AUTH_HEADER, action.payload.token)
