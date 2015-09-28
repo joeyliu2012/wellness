82e80f01100e44dd507612f3113373f75bb27da2
Zach Perrault <zach.perrault@gmail.com>
2015-09-28 12:48:02 -0400

    Change action type name

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,10 +1,10 @@
 import api from 'utils/api'
 
-import { REQUEST_LOGIN, RECEIVE_TOKEN } from 'constants/action-types'
+import { REQUEST_TOKEN, RECEIVE_TOKEN } from 'constants/action-types'
 
 function requestLogin() {
   return {
-    type: REQUEST_LOGIN,
+    type: REQUEST_TOKEN,
   }
 }
 
