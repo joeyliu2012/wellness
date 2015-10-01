3286016d01dacdef39b1076032ec5b3b3b728424
Zach Perrault <zach.perrault@gmail.com>
2015-10-01 12:47:17 -0400

    bindActionCreators was unused

--- src/components/web/login-form.web.js
+++ src/components/web/login-form.web.js
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 
-import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import { fetchAuthToken } from 'actions/token'
