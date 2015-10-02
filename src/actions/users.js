c74c2cfd9c05472960748e47242ba9476f744ff1
Wilson Taylor <wilsonmct55@gmail.comg>
2015-10-02 00:02:24 -0400

    More linter resolutions.

--- src/actions/users.js
+++ src/actions/users.js
@@ -2,17 +2,17 @@ import { makeApiRequest } from 'actions/api'
 import { receiveToken } from 'actions/auth'
 import { RECEIVE_CURRENT_USER } from 'constants/action-types'
 
-export function userAndTokenReceived(data) {
-  return (dispatch) => {
-      dispatch(receiveCurrentUser(data))
-      dispatch(receiveToken(data))
-  }
-}
-
 export function receiveCurrentUser(user) {
   return {
     type: RECEIVE_CURRENT_USER,
     payload: user,
+  }
+}
+
+export function userAndTokenReceived(data) {
+  return (dispatch) => {
+    dispatch(receiveCurrentUser(data))
+    dispatch(receiveToken(data))
   }
 }
 
