8acdeb289048adac5531f2cb15e157fe1bb069f1
Wilson Taylor <wilsonmct55@gmail.comg>
2015-10-01 22:46:05 -0400

    Resolving linter issues

--- src/components/web/signup-form.web.js
+++ src/components/web/signup-form.web.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 
 import { createUserInDatabase } from 'actions/users'
@@ -14,6 +14,12 @@ const INITIAL_STATE = {
   { createUserInDatabase }
 )
 export default class SignupForm extends Component {
+
+  static propTypes = {
+    createUserInDatabase: PropTypes.func.isRequired,
+    currentUser: Prop.number.isRequired,
+  }
+
   constructor(props, context) {
     super(props, context)
     this.state = INITIAL_STATE
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -4,14 +4,14 @@ export default function users(state = {
   currentUser: null,
 }, action) {
   switch (action.type) {
-    case CREATE_USER:
-      return state
-    case USER_CREATED:
-      return {
-        ...state,
-        currentUser: action.payload
-      }
-    default:
-      return state
+  case CREATE_USER:
+    return state
+  case USER_CREATED:
+    return {
+      ...state,
+      currentUser: action.payload,
+    }
+  default:
+    return state
   }
 }
