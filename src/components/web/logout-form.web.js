ef519977ff2831f3e92e817d2a0e6a7af051d1e0
Charlie Murphy <tm507211@ohio.edu>
2015-10-06 17:25:08 -0400

    recreated logout component and added to home component

---
+++ src/components/web/logout-form.web.js
@@ -0,0 +1,37 @@
+import React, { Component, PropTypes } from 'react'
+import { Link } from 'react-router'
+
+import { connect } from 'react-redux'
+import { deleteAuthToken from 'actions/auth'
+
+function mapStateToProps(state) {
+  return state.token
+}
+
+const mapDispatchToProps = { logout : deleteAuthToken {
+
+@connect(
+  mapStateToProps,
+  mapDispatchToProps,)
+export default class LogoutForm extends Component {
+  static propTypes = {
+    logout: PropTypes.func.isRequired,
+  }
+
+  constructor(props, context) {
+    super(props, context)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(evt) {
+    this.props.logout()
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        Logout
+      </button>
+    )
+  }
+}
